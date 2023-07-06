7b17952ccab398a535757ff54ec8c1bf7ca0b922
Luiz Fernando <luizfernandorg@gmail.com>
2023-07-06 11:18:39 -0300

    add Bootstrap Modal

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './TaskItem.css'
 
 const TaskItem = (props) => {
-    const [taskTitle, setTaskTitle] = useState(props.task.taskTitle)
-    const [taskDescription, setTaskDescription] = useState(props.task.taskDescription)
+    // const [taskTitle, setTaskTitle] = useState(props.task.taskTitle)
+    // const [taskDescription, setTaskDescription] = useState(props.task.taskDescription)
 
-    const [edit, setEdit] = useState(false)
+    // const [edit, setEdit] = useState(false)
 
     const onEdit = () => {
-        setEdit(true)
+        props.onShowModal(props.task)
+        // setEdit(true)
     }
 
     const onRemove = () => {
@@ -28,53 +29,53 @@ const TaskItem = (props) => {
          })
     }
 
-    const onChangeTitle = (event) => {
-        setTaskTitle(event.target.value)
-    }
+    // const onChangeTitle = (event) => {
+    //     setTaskTitle(event.target.value)
+    // }
 
-    const onChangeDescription = (event) => {
-        setTaskDescription(event.target.value)
-    }
+    // const onChangeDescription = (event) => {
+    //     setTaskDescription(event.target.value)
+    // }
     
-    const onSubmitTask = (event) => {
-        event.preventDefault()
-        fetch('http://localhost:3000',{
-            method: 'put',
-            headers: {'ACCEPT': 'application/json', 'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                taskID: parseInt(props.task.taskID),
-                taskTitle: taskTitle,
-                taskDescription: taskDescription
-            })
-        }).then( async (response) => {
-            return await response.json()
-        }).then( async (data) => {
-            console.log(data)
-            await props.update()
-            setEdit(false)
-        }).catch( err => {
-            console.log(err)
-        })
+    // const onSubmitTask = (event) => {
+    //     event.preventDefault()
+    //     fetch('http://localhost:3000',{
+    //         method: 'put',
+    //         headers: {'ACCEPT': 'application/json', 'Content-Type': 'application/json'},
+    //         body: JSON.stringify({
+    //             taskID: parseInt(props.task.taskID),
+    //             taskTitle: taskTitle,
+    //             taskDescription: taskDescription
+    //         })
+    //     }).then( async (response) => {
+    //         return await response.json()
+    //     }).then( async (data) => {
+    //         console.log(data)
+    //         await props.update()
+    //         setEdit(false)
+    //     }).catch( err => {
+    //         console.log(err)
+    //     })
         
-    }
+    // }
 
-    if(edit) {
-        return (
-            <div key={props.task.taskID} className='editForm'>
-                <form onSubmit={onSubmitTask}>
-                    <div className='blockEditInput'>
-                        <label htmlFor='inputText'>Title</label>
-                        <input type='text' id="inputText" value={taskTitle} onChange={onChangeTitle}/>
-                    </div>
-                    <div className='blockEditInput'>
-                        <label htmlFor="inputDescription">Description</label>
-                        <input type='text' id="inputDescription" value={taskDescription} onChange={onChangeDescription}/>
-                    </div>
-                    <button className='submitButton' type='submit'>Salvar</button>
-                </form> 
-            </div>
-        )
-    } else {
+    // if(edit) {
+    //     return (
+    //         <div key={props.task.taskID} className='editForm'>
+    //             <form onSubmit={onSubmitTask}>
+    //                 <div className='blockEditInput'>
+    //                     <label htmlFor='inputText'>Title</label>
+    //                     <input type='text' id="inputText" value={taskTitle} onChange={onChangeTitle}/>
+    //                 </div>
+    //                 <div className='blockEditInput'>
+    //                     <label htmlFor="inputDescription">Description</label>
+    //                     <input type='text' id="inputDescription" value={taskDescription} onChange={onChangeDescription}/>
+    //                 </div>
+    //                 <button className='submitButton' type='submit'>Salvar</button>
+    //             </form> 
+    //         </div>
+    //     )
+    // } else {
         return (
             <div key={props.task.taskID} className='itemContainer'>
                 <div className='task-details'>
@@ -95,6 +96,6 @@ const TaskItem = (props) => {
             </div>
         )
     }
-}
+// }
 
 export default TaskItem
--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -3,6 +3,50 @@ import TaskItem from './TaskItem'
 import './TaskList.css'
 
 const TaskList = (props) => {
+    const [editTask, setEditTask] = useState({})
+
+    const showModal = (task) => {
+        setEditTask((prev) => {
+            return {...task}
+        })
+        new bootstrap.Modal('#staticBackdrop').show()
+    }
+    
+    const onChangeTitle = (event) => {
+        setEditTask( (prev) => {
+            const taskTitle = event.target.value 
+            return {...prev,taskTitle}
+        })
+    }
+
+    const onChangeDescription = (event) => {
+        setEditTask( (prev) => {
+            const taskDescription = event.target.value
+            return {...prev,taskDescription}
+        })
+    }
+    const saveTask = () => {
+        fetch('http://localhost:3000',{
+            method: 'put',
+            headers: {'ACCEPT': 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                taskID: parseInt(editTask.taskID),
+                taskTitle: editTask.taskTitle,
+                taskDescription: editTask.taskDescription
+            })
+        }).then( async (response) => {
+            return await response.json()
+        }).then( async (data) => {
+            console.log(data)
+            await props.update()
+            // setEdit(false)
+            new bootstrap.Modal('#staticBackdrop').hide()
+            setEditTask({})
+        }).catch( err => {
+            console.log(err)
+        })
+    }
+
     if(props.tasks.length === 0){
         return (
             <span>
@@ -14,9 +58,35 @@ const TaskList = (props) => {
             <>
                 {
                     props.tasks.map( (task) => {
-                        return <TaskItem key={task.taskID} update={props.update} task={task}/>
+                        return <TaskItem key={task.taskID} update={props.update} task={task} onShowModal={showModal}/>
                     })
                 }
+                
+                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                    <div className='blockEditInput'>
+                        <label htmlFor='inputText'>Title</label>
+                        <input type='text' id="inputText" value={editTask.taskTitle} onChange={onChangeTitle}/>
+                    </div>
+                    <div className='blockEditInput'>
+                        <label htmlFor="inputDescription">Description</label>
+                        <input type='text' id="inputDescription" value={editTask.taskDescription} onChange={onChangeDescription}/>
+                    </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={saveTask}>Save</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
            </>
         )
 
